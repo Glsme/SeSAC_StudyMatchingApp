08de7113f7a030e14171a95080f1e8b2bd5e0f90
Glsme <glasses.str.man@gmail.com>
2022-11-13 19:12:44 +0900

    Test: TabBar Test code 추가

--- SeSAC_StudyMatchingApp/Presentation/Main/MainTabBarController.swift
+++ SeSAC_StudyMatchingApp/Presentation/Main/MainTabBarController.swift
@@ -15,9 +15,12 @@ class MainTabBarController: UITabBarController {
         tabBar.backgroundColor = .white
         
         let homeVC = HomeViewController()
-        let test = HomeViewController()
+        let test1 = HomeViewController()
+        let test2 = HomeViewController()
+        let test3 = HomeViewController()
+        let test4 = HomeViewController()
         
-        setViewControllers([homeVC, test], animated: false)
+        setViewControllers([homeVC, test1, test2, test3, test4], animated: false)
         
         if let items = tabBar.items {
             items[0].selectedImage = UIImage (systemName: "folder.fill")
@@ -27,6 +30,18 @@ class MainTabBarController: UITabBarController {
             items[1].selectedImage = UIImage (systemName: "star.fill")
             items[1].image = UIImage (systemName: "star")
             items[1].title="즐겨찾기"
+            
+            items[2].selectedImage = UIImage (systemName: "star.fill")
+            items[2].image = UIImage (systemName: "star")
+            items[2].title="즐겨찾기"
+            
+            items[3].selectedImage = UIImage (systemName: "star.fill")
+            items[3].image = UIImage (systemName: "star")
+            items[3].title="즐겨찾기"
+            
+            items[4].selectedImage = UIImage (systemName: "star.fill")
+            items[4].image = UIImage (systemName: "star")
+            items[4].title="즐겨찾기"
         }
     }
 }
