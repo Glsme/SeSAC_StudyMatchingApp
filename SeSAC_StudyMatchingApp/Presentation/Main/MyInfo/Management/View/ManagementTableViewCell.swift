334d54ed5c96a8472012769e009e031da890d5c5
Glsme <glasses.str.man@gmail.com>
2022-11-15 15:48:46 +0900

    Refactor: ManagementTableViewCell 교체

--- SeSAC_StudyMatchingApp/Presentation/Main/MyInfo/Management/View/ManagementTableViewCell.swift
+++ SeSAC_StudyMatchingApp/Presentation/Main/MyInfo/Management/View/ManagementTableViewCell.swift
@@ -13,6 +13,8 @@ class ManagementTableViewCell: UITableViewCell {
     lazy var genderView = CustomGenderView()
     lazy var studyView = CustomStudyView()
     lazy var phoneView = CustomPhoneView()
+    lazy var ageView = CustomAgeView()
+    lazy var withdrawView = CustomWithdrawView()
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
@@ -26,12 +28,45 @@ class ManagementTableViewCell: UITableViewCell {
     }
     
     func configureUI() {
-        [genderView, studyView, phoneView].forEach {
+        [genderView, studyView, phoneView, ageView, withdrawView].forEach {
             self.contentView.addSubview($0)
         }
     }
     
     func setConstraints() {
+        genderView.snp.makeConstraints { make in
+            make.top.equalTo(self.snp.top)
+            make.leading.equalTo(self.snp.leading)
+            make.trailing.equalTo(self.snp.trailing)
+            make.height.equalTo(75)
+        }
         
+        studyView.snp.makeConstraints { make in
+            make.top.equalTo(genderView.snp.bottom)
+            make.trailing.equalTo(genderView.snp.trailing)
+            make.leading.equalTo(genderView.snp.leading)
+            make.height.equalTo(75)
+        }
+        
+        phoneView.snp.makeConstraints { make in
+            make.top.equalTo(studyView.snp.bottom)
+            make.trailing.equalTo(studyView.snp.trailing)
+            make.leading.equalTo(studyView.snp.leading)
+            make.height.equalTo(75)
+        }
+        
+        ageView.snp.makeConstraints { make in
+            make.top.equalTo(phoneView.snp.bottom)
+            make.trailing.equalTo(phoneView.snp.trailing)
+            make.leading.equalTo(phoneView.snp.leading)
+            make.height.equalTo(110)
+        }
+        
+        withdrawView.snp.makeConstraints { make in
+            make.top.equalTo(ageView.snp.bottom)
+            make.trailing.equalTo(ageView.snp.trailing)
+            make.leading.equalTo(ageView.snp.leading)
+            make.height.equalTo(75)
+        }
     }
 }
