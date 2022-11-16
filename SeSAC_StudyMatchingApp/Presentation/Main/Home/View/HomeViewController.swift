483b974a77208eaf6d17530811ab2407b9e7a74e
Glsme <glasses.str.man@gmail.com>
2022-11-16 20:14:01 +0900

    Feat: 홈 UI 위치 관련 코드 추가

--- SeSAC_StudyMatchingApp/Presentation/Main/Home/View/HomeViewController.swift
+++ SeSAC_StudyMatchingApp/Presentation/Main/Home/View/HomeViewController.swift
@@ -5,11 +5,17 @@
 //  Created by Seokjune Hong on 2022/11/11.
 //
 
+import CoreLocation
+import MapKit
 import UIKit
 
 final class HomeViewController: BaseViewController {
     let mainView = HomeView()
     let viewModel = MainViewModel()
+    
+    let locationManager = CLLocationManager()
+    var locationStatus = false
+    let defaultCoordinate = CLLocationCoordinate2D(latitude: 37.517819364682694, longitude: 126.88647317074734)
     
     override func loadView() {
         self.view = mainView
@@ -18,5 +24,68 @@ final class HomeViewController: BaseViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        locationManager.delegate = self
+        checkUserDeviceLocationServiceAuthorization()
+    }
+    
+    override func configureUI() {
+        
+    }
+    
+    func setUserRegionAndAnnotation(center: CLLocationCoordinate2D) {
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 10000, longitudinalMeters: 10000)
+        mainView.mapView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = center
+        annotation.title = "현재 위치"
+        
+        mainView.mapView.addAnnotation(annotation)
+    }
+}
+
+extension HomeViewController {
+    func checkUserDeviceLocationServiceAuthorization() {
+        let authorizationStatus: CLAuthorizationStatus
+        
+        
+        authorizationStatus = locationManager.authorizationStatus
+        checkUserCurrentLocationAuthorization(authorizationStatus)
+    }
+    
+    func checkUserCurrentLocationAuthorization(_ authorizationStatus: CLAuthorizationStatus) {
+        switch authorizationStatus {
+        case .notDetermined:
+            print("NOT DETERMINED")
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("DENIED, 아이폰 설정으로 유도합니다.")
+        case .authorizedWhenInUse:
+            print("WHEN IN USE")
+            locationManager.startUpdatingLocation()
+            locationStatus = true
+        default:
+            print("DEFUALT")
+        }
+    }
+}
+
+extension HomeViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let coordinate = locations.first {
+            print(coordinate.coordinate, "!!!!!!!!")
+            setUserRegionAndAnnotation(center: coordinate.coordinate)
+        }
+        
+        locationManager.stopUpdatingHeading()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkUserDeviceLocationServiceAuthorization()
     }
 }
