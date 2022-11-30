1093a9d3b8d378204d2178924b57e7fa99911a6f
Glsme <glasses.str.man@gmail.com>
2022-11-30 18:02:06 +0900

    Feat: Review TextView 바인딩 기능 추가

--- SeSAC_StudyMatchingApp/Presentation/Main/Home/Chatting/Popup/review/View/ReviewViewController.swift
+++ SeSAC_StudyMatchingApp/Presentation/Main/Home/Chatting/Popup/review/View/ReviewViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 
 import RxCocoa
+import RxGesture
 import RxSwift
 
 class ReviewViewController: BaseViewController {
@@ -29,10 +30,38 @@ class ReviewViewController: BaseViewController {
     }
     
     override func bindData() {
+        view.rx.tapGesture()
+            .when(.recognized)
+            .withUnretained(self)
+            .subscribe { (vc, _) in
+                vc.view.endEditing(true)
+            }
+            .disposed(by: disposebag)
+        
         mainView.closeButton.rx.tap
             .withUnretained(self)
             .bind { (vc, _) in
                 vc.dismiss(animated: false)
+            }
+            .disposed(by: disposebag)
+        
+        mainView.reviewButton.rx.tap
+            .withUnretained(self)
+            .bind { (vc, _) in
+                if vc.mainView.reviewButton.backgroundColor == .sesacGreen {
+                    print("hi")
+                } else {
+                    vc.view.makeToast("리뷰 버튼, 혹은 내용을 입력해주세요.", position: .center)
+                }
+            }
+            .disposed(by: disposebag)
+        
+        mainView.reviewTextView.rx.didChange
+            .withUnretained(self)
+            .bind { (vc, text) in
+                print(text)
+                let checkBool: Bool = vc.mainView.checkOtherButtonTabOrTextviewIsNotNil()
+                vc.mainView.reviewButton.setEnabledButton(checkBool)
             }
             .disposed(by: disposebag)
     }
